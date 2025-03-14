6b9ff062ed2042d586732db898c1eddc3c3a01a7
MelquisedequeSilvaVIRTUS <melquisedeque.virtus@gmail.com>
2025-03-14 12:15:52 -0300

    Atualização gpio_irq_manager.h Parte 1
    Adicionei uma pequena parte

--- Button/Examples/Butto_irq_example/inc/gpio_irq_manager.h
+++ Button/Examples/Butto_irq_example/inc/gpio_irq_manager.h
@@ -4,17 +4,24 @@
 #ifndef GPIO_IRQ_MANAGER_H
 #define GPIO_IRQ_MANAGER_H
 
+// Inclusão de bibliotecas necessárias 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include <stdio.h>
 
 #define MAX_GPIO_PINS 30  // Número máximo de pinos GPIO suportados
 
-// Declaração do vetor de callbacks como extern
-extern void (*callbacks[MAX_GPIO_PINS])(void);
+// Declaração do vetor de ponteiros para funções de callbacks como extern
+extern void (*callbacks[MAX_GPIO_PINS])(void); // Armazena os callbacks associados a cada pino GPIO 
 
 
 // Protótipos das funções
+
+/*
+* @brief Manipulador das interrupções para cada GPIO
+* @param gpio Número do pino que gerou a interrupção 
+* @param events Tipo de evento que ocorreu (Ex: Borda de subida/descida)
+*/ 
 void gpio_irq_handler(uint gpio, uint32_t events);
 void register_gpio_callback(uint gpio, void (*callback)(void),uint32_t event_mask);
 void remove_gpio_callback(uint gpio,uint32_t event_mask);
